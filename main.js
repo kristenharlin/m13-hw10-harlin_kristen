6f277b3d2a3b6ad12f4dbeae86606094a8121631
Kristen Harlin <kristenharlin@Kristens-MacBook-Pro.local>
2018-11-18 08:31:28 -0500

    Comment out vanilla javascript

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //JavaScript Functionality Part 1 Goes Here. Comment it all out once finished.
 
-var section = document.querySelector('section');
-var button = document.querySelector('button');
+//var section = document.querySelector('section');
+//var button = document.querySelector('button');
 
-var slideToggle = function () {
-  section.classList.toggle('hide');
-}
+//var slideToggle = function () {
+//  section.classList.toggle('hide');
+//}
 
-button.addEventListener('click', slideToggle);
+//button.addEventListener('click', slideToggle);
 
 //jQuery functionality will go here. Make sure to iclude the document.ready function.
