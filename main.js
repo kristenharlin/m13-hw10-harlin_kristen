ca84032bd9722df1dc95980104056aa4ec8394e8
Kristen Harlin <kristenharlin@Kristens-MacBook-Pro.local>
2018-11-18 08:28:40 -0500

    Add variables section and button

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-//JavaScript Functionalit Part 1 Goes Here. Comment it all out once finsihed.
+//JavaScript Functionality Part 1 Goes Here. Comment it all out once finished.
 
-
+var section = document.querySelector('section');
+var button = document.querySelector('button');
 
 
 //jQuery functionality will go here. Make sure to iclude the document.ready function.
